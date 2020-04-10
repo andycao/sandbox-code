f8df6f87706897078ea247ac5d2413e613c56f86
andy cao <andycao@users.noreply.github.com>
2020-04-10 15:32:56 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React, { useReducer, useRef } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+function ShoppingList() {
+  const inputRef = useRef();
+  const [items, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "add":
+        return [
+          ...state,
+          {
+            id: state.length,
+            name: action.name
+          }
+        ];
+      case "remove":
+        // keep every item except the one we want to remove
+        return state.filter((_, index) => index !== action.index);
+      case "clear":
+        return [];
+      default:
+        return state;
+    }
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({
+      type: "add",
+      name: inputRef.current.value
+    });
+    inputRef.current.value = "";
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input ref={inputRef} />
+      </form>
+      <ul>
+        {items.map((item, index) => (
+          <li key={item.id}>
+            {item.name}{" "}
+            <button onClick={() => dispatch({ type: "remove", index })}>
+              X
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => dispatch({ type: "clear" })}>clear</button>
+    </>
+  );
+}
+
+ReactDOM.render(<ShoppingList />, document.querySelector("#root"));
